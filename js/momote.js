09e53fd9a8cb6901cdb1e36f193bea0bd9d6b8cf
Bruno-dev1 <brunonasifalso@gmail.com>
2024-12-04 23:33:08 -0300

    Initial commit

---
+++ js/momote.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("form")
+const code = document.querySelector("#outCode")
+const desc = document.querySelector("#outDesc")
+const hobby = document.querySelector("#outHobby")
+const img = document.querySelector("img")
+frm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const nome = frm.inCode.value
+    if (nome =="BNS2211"){
+        code.innerHTML = "BRUNO NASCIMENTO SANTIAGO(fofoso)"
+        desc.innerHTML = "DESCRIÇÂO: Estudante de TI, e ama programar e ficar com a namorada \n "
+        hobby.innerHTML = "HOBBY : Programar,cozinhar,ler e sentir saudades da mo dele(Isabelly Kely)"
+        img.setAttribute("src","js/bruno.jpg")
+    }else{
+        code.innerText = "Nome Invalido, quem é esse?😒"}
+})
