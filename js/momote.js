1898c60ee6376efe3cfea5f6b3b79d2d92eef43c
Bruno-dev1 <brunonasifalso@gmail.com>
2024-12-04 23:54:02 -0300

    Homenagem

--- js/momote.js
+++ js/momote.js
@@ -2,6 +2,7 @@ const frm = document.querySelector("form")
 const code = document.querySelector("#outCode")
 const desc = document.querySelector("#outDesc")
 const hobby = document.querySelector("#outHobby")
+const video = document.querySelector("#video")
 const img = document.querySelector("img")
 frm.addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -11,6 +12,8 @@ frm.addEventListener("submit",(e)=>{
         desc.innerHTML = "DESCRIÇÂO: Estudante de TI, e ama programar e ficar com a namorada \n "
         hobby.innerHTML = "HOBBY : Programar,cozinhar,ler e sentir saudades da mo dele(Isabelly Kely)"
         img.setAttribute("src","js/bruno.jpg")
+        
+        video.innerText = "Homenagem de fofoso"
     }else{
         code.innerText = "Nome Invalido, quem é esse?😒"}
 })
